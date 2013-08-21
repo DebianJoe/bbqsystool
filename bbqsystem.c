a990aef0680a08fa9e66f4c23f93cc26b103a8fa
DebianJoe <jbpaladin27@gmail.com>
2013-08-21 09:48:58 -0500

    still designing

--- bbqsystem.c
+++ bbqsystem.c
@@ -13,13 +13,17 @@ int main (int argc, char *argv[])
 
     char intro[]="BBQ System Tweak Tool\n";
     char opt1[]="Selection1";
-    char opt2[]="Selection2\n";
+    char opt2[]="Selection2";
+    char opt3[]="Whatever";
+    char opt4[]="Junk";
     int left_third = (col-strlen(opt1))/3;
     int right_third = (col-strlen(opt2))/1.5;
 
     mvprintw(0, (col-strlen(intro))/2, "%s",intro); 
-    mvprintw(1,left_third, "%s",opt1);
-    mvprintw(1,right_third, "%s", opt2);
+    mvprintw(2,left_third, "%s",opt1);
+    mvprintw(2,right_third, "%s", opt2);
+    mvprintw(3,left_third, "%s", opt3);
+    mvprintw(3,right_third, "%s", opt4);
     ch = getch();   //this is why we call raw(), to not have
                     //have to press "return" after every 
                     //keypress
