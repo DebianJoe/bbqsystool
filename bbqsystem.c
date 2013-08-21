2257b9247fcab791fca7db37904970b681050b46
DebianJoe <jbpaladin27@gmail.com>
2013-08-21 11:01:07 -0500

    design, design

--- bbqsystem.c
+++ bbqsystem.c
@@ -2,24 +2,35 @@
 #include <stdio.h>
 #include <string.h>
 
-int main (int argc, char *argv[])
+WINDOW *create_newwin(int height, int width, int starty, int startx);
+void destroy_win(WINDOW *local_win);
+
+int main (int argc, char *argv[]) //thinking about switches, perhaps colorless?
 {
-  int ch, row, col;         //declare ch
+    int ch, row, col;       //declare ch
+    int startx, starty, width, height;
     initscr();              //initialize curses
     getmaxyx(stdscr,row,col);
     raw();                  //line buffering disabled
     keypad(stdscr, TRUE);   //F1, F2, etc activated
     noecho();
+    start_color();
+
+    height = 30;
+    width = 10;
+    starty = (LINES - height) / 2;
+    startx = (COLS - width) / 2;
 
     char intro[]="BBQ System Tweak Tool\n";
     char opt1[]="Selection1";
     char opt2[]="Selection2";
     char opt3[]="Whatever";
     char opt4[]="Junk";
+    int centered = (col-strlen(intro))/2;
     int left_third = (col-strlen(opt1))/3;
     int right_third = (col-strlen(opt2))/1.5;
 
-    mvprintw(0, (col-strlen(intro))/2, "%s",intro); 
+    mvprintw(0,centered, "%s",intro); 
     mvprintw(2,left_third, "%s",opt1);
     mvprintw(2,right_third, "%s", opt2);
     mvprintw(3,left_third, "%s", opt3);
