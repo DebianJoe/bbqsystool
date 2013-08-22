41ffdc62734b7f805792105d95204b4266b13e35
DebianJoe <jbpaladin27@gmail.com>
2013-08-22 01:05:56 -0500

    start cases

--- bbqsystem.c
+++ bbqsystem.c
@@ -7,6 +7,7 @@ void destroy_win(WINDOW *local_win);
 
 int main (int argc, char *argv[]) //thinking about switches, perhaps colorless?
 {
+    WINDOW *main_win;
     int ch, row, col;       //declare ch
     int startx, starty, width, height;
     initscr();              //initialize curses
@@ -16,16 +17,18 @@ int main (int argc, char *argv[]) //thinking about switches, perhaps colorless?
     noecho();
     start_color();
 
-    height = 30;
-    width = 10;
+    height = 10;
+    width = 50;
     starty = (LINES - height) / 2;
     startx = (COLS - width) / 2;
 
     char intro[]="BBQ System Tweak Tool\n";
-    char opt1[]="Selection1";
+    char opt1[]="BBQ CPU";
     char opt2[]="Selection2";
     char opt3[]="Whatever";
     char opt4[]="Junk";
+    char exit[]="Press F12 to exit";
+    char help[]="Press F1 for  Help";
     int centered = (col-strlen(intro))/2;
     int left_third = (col-strlen(opt1))/3;
     int right_third = (col-strlen(opt2))/1.5;
@@ -35,21 +38,28 @@ int main (int argc, char *argv[]) //thinking about switches, perhaps colorless?
     mvprintw(2,right_third, "%s", opt2);
     mvprintw(3,left_third, "%s", opt3);
     mvprintw(3,right_third, "%s", opt4);
+    mvprintw(7,centered, "%s", exit);
+    mvprintw(6,centered, "%s", help);
     ch = getch();   //this is why we call raw(), to not have
                     //have to press "return" after every 
                     //keypress
     if(ch == KEY_F(1)){
-        printw("This is where I'll put some help");
+        mvprintw(8,0,"This is where I'll put some help");
     }
     //obviously need to spawn some windows and add logic
     else{
-        printw("we'll echo our choices here\n");
+        mvprintw(8,0,"we'll echo our choices here\n");
         attron(A_BOLD);
         printw("%c", ch);
         attroff(A_BOLD);
     }
     refresh();
     getch();
+    while((ch =getch()) != KEY_F(12))
+    {{//switch(ch)
+       // {   case KEY(1):
+        getch();
+         }}
     endwin();
 
     return 0;
