0d08fae1679d95ed3a19e5fdbc68df20527104b1
DebianJoe <jbpaladin27@gmail.com>
2013-08-22 01:28:34 -0500

    started loops

--- bbqsystem.c
+++ bbqsystem.c
@@ -1,6 +1,7 @@
 #include <ncurses.h>
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 
 WINDOW *create_newwin(int height, int width, int starty, int startx);
 void destroy_win(WINDOW *local_win);
@@ -15,7 +16,13 @@ int main (int argc, char *argv[]) //thinking about switches, perhaps colorless?
     raw();                  //line buffering disabled
     keypad(stdscr, TRUE);   //F1, F2, etc activated
     noecho();
+    if(has_colors()== FALSE)
+    {   endwin();
+        printf("Your Terminal does not support color\n");
+        exit(1);
+    }
     start_color();
+    init_pair(1, COLOR_RED, COLOR_BLACK);
 
     height = 10;
     width = 50;
@@ -27,23 +34,32 @@ int main (int argc, char *argv[]) //thinking about switches, perhaps colorless?
     char opt2[]="Selection2";
     char opt3[]="Whatever";
     char opt4[]="Junk";
-    char exit[]="Press F12 to exit";
+    char key_exit[]="Press F12 to exit";
     char help[]="Press F1 for  Help";
     int centered = (col-strlen(intro))/2;
     int left_third = (col-strlen(opt1))/3;
     int right_third = (col-strlen(opt2))/1.5;
 
+    attron(A_BOLD);
+    attron(COLOR_PAIR(1));
     mvprintw(0,centered, "%s",intro); 
+    attroff(COLOR_PAIR(1));
+    attroff(A_BOLD);
     mvprintw(2,left_third, "%s",opt1);
     mvprintw(2,right_third, "%s", opt2);
     mvprintw(3,left_third, "%s", opt3);
     mvprintw(3,right_third, "%s", opt4);
-    mvprintw(7,centered, "%s", exit);
+    mvprintw(7,centered, "%s", key_exit);
     mvprintw(6,centered, "%s", help);
     ch = getch();   //this is why we call raw(), to not have
                     //have to press "return" after every 
                     //keypress
-    if(ch == KEY_F(1)){
+    if(ch == KEY_F(12)){
+        endwin();
+        printf("Happy Roasting\n");
+        exit(1);
+    }
+    else if(ch == KEY_F(1)){
         mvprintw(8,0,"This is where I'll put some help");
     }
     //obviously need to spawn some windows and add logic
